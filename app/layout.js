94c1437a92790579abd17e0d309bff04a3ea5774
Abdullah <95abdullah99@gmail.com>
2025-02-16 11:41:21 +0530

    Enhance authentication pages with centered layout and integrate user provider for new user checks

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
+import Provider from "./provider";
 
 const outfit = Outfit({subsets:['latin']});
 
@@ -13,10 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
-      <body
-        className={outfit.className}
-      >
+      <body className={outfit.className}>
+        <Provider>
         {children}
+        </Provider>
       </body>
     </html>
     </ClerkProvider>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
 <div>
   <h2>Hellooow</h2>
   <Button>Submit</Button>
-  <Button>Hello</Button>
+  <UserButton/>
 </div>
   );
 }
